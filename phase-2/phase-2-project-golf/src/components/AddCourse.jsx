43c81c3e20891d2a83653f7cbfdb999197f9eff2
Andrew <azehnder2@gmail.com>
2022-07-26 15:19:40 -0700

    Adding AddCourse fields

--- phase-2/phase-2-project-golf/src/components/AddCourse.jsx
+++ phase-2/phase-2-project-golf/src/components/AddCourse.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState} from 'react';
 
 const AddCourse = () => {
+    const [course, setCourse] = useState([])
+
+
   return (
-    <div>AddCourse</div>
+    <div>
+        <h1>AddCourse</h1>
+
+        <form>
+            <div>
+                <label htmlFor="course">Course: </label>
+                <input type="text" name="course" id="course" />
+
+                <label htmlFor="location">Location: </label>
+                <input type="text" name="location" id="location" />
+
+                <label htmlFor="price">Price: </label>
+                <input type="text" name="price" id="price" />
+            </div>
+
+            <input type="submit" value="Add Course" />
+
+        </form>
+
+
+    </div>
   )
 }
 
--- phase-2/phase-2-project-golf/src/components/NavBar.jsx
+++ phase-2/phase-2-project-golf/src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
   return (
     <div>
+        <h3>Arizona Golf Course List</h3>
         <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/courses">Course List</Link></li>

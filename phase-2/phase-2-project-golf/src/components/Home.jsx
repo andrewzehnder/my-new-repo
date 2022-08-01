1fd91bfe2961b0ee42f59856758d22a2a7408e7d
Andrew <azehnder2@gmail.com>
2022-08-01 13:48:58 -0500

    Adding material UI to home page.

--- phase-2/phase-2-project-golf/src/components/Home.jsx
+++ phase-2/phase-2-project-golf/src/components/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { styled } from '@mui/material/styles';
 
 const Home = () => {
 
-
+  const Div = styled('div')(({ theme }) => ({
+    ...theme.typography.button,
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(1),
+  }));
 
   return (
-    <div>
+    <Div>
         <h1>Home Page</h1>
-    </div>
+    </Div>
   )
 }
 
